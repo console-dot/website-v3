8d6365e14bf194354249445836cd4861504d8a40
Shuja Ur Rehman <sabeeharif@hotmail.com>
2024-04-23 18:10:42 +0500

    responsiveness

--- src/components/Careers/Checks.jsx
+++ src/components/Careers/Checks.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { FaAngleDown } from "react-icons/fa6";
 
-export const Checks = ({data,heading}) => {
+export const Checks = ({ data, heading }) => {
   const [rotated, setRotated] = useState(false);
 
   const handleClick = () => {
@@ -20,26 +20,30 @@ export const Checks = ({data,heading}) => {
           />
         </div>
       </div>
-      {rotated && <>
-      {data?.map((i)=>
-      <div>
-        <div class="flex items-center gap-2">
-          <input
-            id="bordered-checkbox-1"
-            type="checkbox"
-            value=""
-            name="bordered-checkbox"
-            class="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-          />
-          <label
-            for="bordered-checkbox-1"
-            class="w-full py-4 ms-2 text-[16px] font-medium text-webDescrip"
-          >
-            {i.title}<span className="pl-2">{`(${i.numbers})`}</span>
-          </label>
-          
-        </div>
-      </div>)}</>}
+      {rotated && (
+        <>
+          {data?.map((i) => (
+            <div>
+              <div class="flex items-center gap-2">
+                <input
+                  id="bordered-checkbox-1"
+                  type="checkbox"
+                  value=""
+                  name="bordered-checkbox"
+                  class="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
+                <label
+                  for="bordered-checkbox-1"
+                  class="w-full py-4 ms-2 text-[16px] font-medium text-webDescrip"
+                >
+                  {i.title}
+                  <span className="pl-2">{`(${i.numbers})`}</span>
+                </label>
+              </div>
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,9 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { BrowserRouter as Router, Link, useLocation } from "react-router-dom";
 import { logo } from "../../assets/images";
 import { RiArrowDropDownLine, RiArrowDropUpLine } from "react-icons/ri";
-
+import { FaAngleDown } from "react-icons/fa6";
+import { IoCloseOutline } from "react-icons/io5";
 export const NavBar = ({ section, setSection }) => {
   const navBar = [
     { name: "Home", link: "/" },
@@ -30,8 +31,10 @@ export const NavBar = ({ section, setSection }) => {
     { name: "Case Studies", link: "/case-study" },
     { name: "Careers", link: "/careers" },
   ];
+ 
 
   const [showNav, setShowNav] = useState(false);
+  const [rotated, setRotated] = useState(false);
   const [dropdownStates, setDropdownStates] = useState(navBar.map(() => false));
   const dropdownRefs = useRef(navBar.map(() => React.createRef())); // Refs for the dropdowns
   const location = useLocation();
@@ -58,6 +61,9 @@ export const NavBar = ({ section, setSection }) => {
     }
   };
 
+  const handleClick = () => {
+    setRotated(!rotated);
+  };
   return (
     <div
       style={{
@@ -100,7 +106,7 @@ export const NavBar = ({ section, setSection }) => {
         ></div>
       </div>
       <div className="w-2/3 items-center justify-center flex">
-        <div className="lg:hidden ml-auto" onClick={toggleNav}>
+        {showNav ? <div className="w-full flex justify-end mr-4 "> <IoCloseOutline onClick={toggleNav} className="text-white text-[28px]"/> </div> :<div className="lg:hidden ml-auto" onClick={toggleNav}>
           <svg
             className="w-6 h-6 text-white mr-4 cursor-pointer"
             fill="none"
@@ -115,15 +121,18 @@ export const NavBar = ({ section, setSection }) => {
               d="M4 6h16M4 12h16M4 18h16"
             />
           </svg>
-        </div>
+        </div>}
         {/* Navigation items for desktop */}
         <ul
           className={`lg:flex items-center lg:flex-row lg:text-white text-white lg:justify-around lg:font-sans lg:gap-8 hidden`}
         >
           {navBar?.map((item, index) => (
-            <li key={index} className="hover:bg-[rgb(60,90,133,0.5)]  hover:text-[rgb(98,192,209)]">
+            <li
+              key={index}
+              className="hover:bg-[rgb(60,90,133,0.5)]  hover:text-[rgb(98,192,209)]"
+            >
               {item.dropdown ? (
-                <div className="m-2" ref={dropdownRefs.current[index]} >
+                <div className="m-2" ref={dropdownRefs.current[index]}>
                   <span
                     className={`cursor-pointer flex items-center `}
                     onClick={() => toggleDropdown(index)}
@@ -138,7 +147,10 @@ export const NavBar = ({ section, setSection }) => {
                     </div>
                   </span>
                   {dropdownStates[index] && (
-                    <ul className="absolute bg-custom-purple shadow-md mt-[18px]" onMouseLeave={() => handleMouseLeave(index)}>
+                    <ul
+                      className="absolute bg-custom-purple shadow-md mt-[18px]"
+                      onMouseLeave={() => handleMouseLeave(index)}
+                    >
                       {item.dropdown.map((subitem, subindex) => (
                         <li key={subindex} className="p-2 ">
                           <Link
@@ -176,20 +188,49 @@ export const NavBar = ({ section, setSection }) => {
         <div
           className={`${
             showNav ? "block" : "hidden"
-          } lg:hidden absolute bg-gradient-to-r from-custom-blue to-custom-purple top-[60px] left-0 w-full z-50`}
+          } lg:hidden fixed bg-gradient-to-r  from-custom-purple to-custom-blue top-[60px] right-0 w-[70%] h-[100vh] z-50`}
         >
           <ul
-            onClick={() => setShowNav(false)}
-            className="flex flex-col text-white font-bold gap-4 p-4 justify-center items-center"
+            className="flex flex-col text-white font-bold gap-2 justify-start items-start p-8"
           >
             {navBar.map((item, index) => (
               <li
                 key={index}
-                className={`cursor-pointer hover:bg-gradient-to-r from-custom-blue to-custom-purple ${
+                className={`cursor-pointer ${ item.name != "Services"  ?'hover:bg-gradient-to-r from-custom-blue to-custom-purple' :''} ${
                   location.pathname === item.link ? "text-white" : ""
                 }`}
               >
-                <Link to={item.link}>{item.name}</Link>
+                {item.name === "Services" ? (
+                  <>
+                    <div className="flex items-center gap-4">
+                      <h2 className="text-[16px] p-2 font-poppins leading-6 text-white font-bold">
+                        Services
+                      </h2>
+                      <div onClick={handleClick} className="cursor-pointer ">
+                        <FaAngleDown
+                          className="text-[12px] text-white"
+                          style={{
+                            transform: rotated
+                              ? "rotate(180deg)"
+                              : "rotate(0deg)",
+                          }}
+                        />
+                      </div>
+                    </div>
+                    {rotated && (
+                      <div className="w-full px-8 bg-none flex flex-col justify-end gap-2">
+                        {" "}
+                        {navBar[2].dropdown?.map((i) => (
+                          <Link onClick={toggleNav} to={i.link} className="text-sm p-2 font-normal">
+                            {i.name}
+                          </Link>
+                        ))}
+                      </div>
+                    )}
+                  </>
+                ) : (
+                  <Link onClick={toggleNav} to={item.link} className="p-2">{item.name}</Link>
+                )}
               </li>
             ))}
           </ul>
