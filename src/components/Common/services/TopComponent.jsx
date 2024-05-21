f4e07463ed9b0a9f97674c6f016c441b5c778f06
Shuja Ur Rehman <shuja0094@gmail.com>
2024-05-21 18:16:51 +0500

    ui/ux responsive

--- src/components/Common/services/TopComponent.jsx
+++ src/components/Common/services/TopComponent.jsx
@@ -7,13 +7,13 @@ export const TopComponent = ({ title, heading, description, image }) => {
     <div className="flex w-full lg:flex-row xl:flex-row 2xl:flex-row md:flex-col sm:flex-col xs:flex-col xxs:flex-col xl:gap-16 2xl:gap-16 lg:gap-16 md:gap-8 sm:gap-4">
       <div className="flex flex-col gap-2 sm:text-center xs:text-center xxs:text-center md:text-center lg:text-start xl:text-start 2xl:text-start w-full md:w-full sm:w-full xs:w-full xxs:w-full">
         <h1 className="text-[16px] leading-5 text-dottedBorder">{title}</h1>
-        <h1 className="text-[28px] leading-10 text-custom-purple sm:text-center xs:text-center xxs:text-center md:text-center lg:text-start xl:text-start 2xl:text-start  w-full font-bold">
+        <h1 className="xl:text-[28px] lg:text-[28px] md:text-[28px] sm:text-[18px] xs:text-[18px] xss:text-[18px] leading-10 text-custom-purple sm:text-center xs:text-center xxs:text-center md:text-center lg:text-start xl:text-start 2xl:text-start  w-full font-bold">
           {heading.toUpperCase()}
         </h1>
         <div className="mb-8 flex w-full sm:justify-center xs:justify-center xxs:justify-center md:justify-center lg:justify-start xl:justify-start 2xl:justify-start">
           <Divider />
         </div>
-        <p className="text-webDescrip text-[16px] leading-6 text-justify">
+        <p className="text-webDescrip xl:text-[16px] lg:text-[16px] md:text-[16px] sm:text-[12px] xs:text-[12px] xss:text-[12px] leading-6 text-justify">
           {description}
         </p>
       </div>
--- src/components/Common/services/WhyChoose.jsx
+++ src/components/Common/services/WhyChoose.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Divider } from "../../../constatnts/Divider";
 import { UserCentricIcon } from "../../../assets/icons";
+import Swiper from "swiper";
+import { SwiperSlide } from "swiper/react";
+import useIsMobile from "../../../utils/hooks/useIsMobile";
 
 export const WhyChoose = ({ heading, data, cardsInRow }) => {
   const gradientStyle = {
     background: "linear-gradient(180deg, #0E7789 0%, #3C5A85 100%)",
   };
+  const isMobile = useIsMobile();
   return (
     <div className="w-full mt-12">
       <div className="flex flex-col text-center gap-2 items-center justify-center w-full">
@@ -15,34 +19,69 @@ export const WhyChoose = ({ heading, data, cardsInRow }) => {
         <h1 className="text-[28px] xs:text-[20px] xxs:text-[16px] leading-10 text-custom-purple font-bold">
           {heading.toUpperCase()}
         </h1>
-        <div className="">
+        <div className="xl:mb-0 lg:mb-0 md:mb-0 sm:mb-8 xs:mb-8 xss:mb-8">
           <Divider />
         </div>
       </div>
-      <div className={`w-full grid ${cardsInRow} gap-8 mt-8`}>
-        {data?.map((i) => (
-          <div className=" flex flex-col bg-white border-2 border-transparent  hover:border-2 hover:border-primaryText hover:border-dashed justify-between px-4">
-            <div className="flex flex-col items-center justify-center p-8 sm:p-4 xs:p-4 xxs:p-2 gap-4">
-              <div style={gradientStyle} className="w-[80px] h-[80px] bg-dottedBorder rounded-full flex items-center justify-center mb-4">
-                {i?.icon}
-              </div>
+      {!isMobile ? (
+        <div className={`w-full grid ${cardsInRow} gap-8 mt-8`}>
+          {data?.map((i) => (
+            <div className=" flex flex-col bg-white border-2 border-transparent  hover:border-2 hover:border-primaryText hover:border-dashed justify-between px-4">
+              <div className="flex flex-col items-center justify-center p-8 sm:p-4 xs:p-4 xxs:p-2 gap-4">
+                <div
+                  style={gradientStyle}
+                  className="w-[80px] h-[80px] bg-dottedBorder rounded-full flex items-center justify-center mb-4"
+                >
+                  {i?.icon}
+                </div>
 
-              <h1 className="text-[16px] text-webHeading leading-6 font-bold">
-                {i.title}
-              </h1>
-              {i?.subHeading && (
-                <h1 className="text-[16px] text-center font-poppins my-1 text-dottedBorder leading-6">
-                  {i.subHeading}
+                <h1 className="text-[16px] text-webHeading leading-6 font-bold">
+                  {i.title}
                 </h1>
-              )}
-              <span className="text-[16px] text-webHeading leading-6 text-justify ">
-                {i.description}
-              </span>
+                {i?.subHeading && (
+                  <h1 className="text-[16px] text-center font-poppins my-1 text-dottedBorder leading-6">
+                    {i.subHeading}
+                  </h1>
+                )}
+                <span className="text-[16px] text-webHeading leading-6 text-justify ">
+                  {i.description}
+                </span>
+              </div>
+              <div className=" w-full h-2 bg-gradient-to-r from-fromclr to-toclr"></div>
             </div>
-            <div className=" w-full h-2 bg-gradient-to-r from-fromclr to-toclr"></div>
+          ))}
+        </div>
+      ) : (
+        <div className="w-full overflow-x-auto flex sm:hidden">
+          <div className="flex gap-8">
+            {data?.map((i) => (
+              <div className="flex flex-col bg-white border-2 border-transparent hover:border-2 hover:border-primaryText hover:border-dashed justify-between px-4 min-w-[300px]">
+                <div className="flex flex-col items-center justify-center p-8 sm:p-4 xs:p-4 xxs:p-2 gap-4">
+                  <div
+                    style={gradientStyle}
+                    className="w-[80px] h-[80px] bg-dottedBorder rounded-full flex items-center justify-center mb-4"
+                  >
+                    {i?.icon}
+                  </div>
+
+                  <h1 className="text-[16px] text-webHeading leading-6 font-bold">
+                    {i.title}
+                  </h1>
+                  {i?.subHeading && (
+                    <h1 className="text-[16px] text-center font-poppins my-1 text-dottedBorder leading-6">
+                      {i.subHeading}
+                    </h1>
+                  )}
+                  <span className="text-[16px] text-webHeading leading-6 text-justify">
+                    {i.description}
+                  </span>
+                </div>
+                <div className="w-full h-2 bg-gradient-to-r from-fromclr to-toclr"></div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </div>
+      )}
     </div>
   );
 };
