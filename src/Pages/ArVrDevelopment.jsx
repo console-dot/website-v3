236f738ae7b4ed4e54a96c386efff3eaf49d9f85
Shuja Ur Rehman <shuja0094@gmail.com>
2024-05-22 13:10:45 +0500

    arvr responsive

--- src/Pages/ArVrDevelopment.jsx
+++ src/Pages/ArVrDevelopment.jsx
@@ -16,7 +16,7 @@ export const ArVrDevelopment = () => {
   return (
     <div className="w-full mb-8">
       <LandingPage data={landingPageDataArVr} />
-      <div className="lg:p-16 xl:p-16 2xl:p-16 sm:p-8 xs:p-4 xxs:p-2">
+      <div className="lg:p-16 xl:p-16 2xl:p-16 sm:p-8 xs:p-8 xss:p-8">
         <div className="pb-4">
           {" "}
           <TopComponent
